9715d22b241ce25ba71cfea4b519b0655168167b
ACK72 <acikom72@gmail.com>
2023-02-22 16:30:30 +0900

    update version to v1.1.0

--- src/Kavya/Kavya.ts
+++ src/Kavya/Kavya.ts
@@ -32,7 +32,7 @@ import {
 import { searchRequest } from './Search';
 
 export const KavyaInfo: SourceInfo = {
-	version: '1.0.0',
+	version: '1.1.0',
 	name: 'Kavya',
 	icon: 'icon.png',
 	author: 'ACK72',
