55557c37f5a606eb383f6ca3b740d63e39c4bdf4
ACK72 <acikom72@gmail.com>
2023-03-20 01:55:37 +0900

    update version to 1.2.5

--- src/Kavya/Kavya.ts
+++ src/Kavya/Kavya.ts
@@ -36,7 +36,7 @@ const sortHelper = (a: any, b: any) => {
 }
 
 export const KavyaInfo: SourceInfo = {
-	version: '1.2.4',
+	version: '1.2.5',
 	name: 'Kavya',
 	icon: 'icon.png',
 	author: 'ACK72',
